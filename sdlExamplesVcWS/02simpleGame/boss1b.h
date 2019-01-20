ca123399666147029841c54f418391dfdcdfc901
AitorAstorga <44289776+AitorAstorga@users.noreply.github.com>
2019-01-20 21:12:20 +0100

    Update boss1b.h

--- sdlExamplesVcWS/02simpleGame/boss1b.h
+++ sdlExamplesVcWS/02simpleGame/boss1b.h
@@ -5,8 +5,6 @@
 
 POSIZIOA ZIRKULUA_JOKALARIARA_MUGITU1(POSIZIOA posizioaPilota, POSIZIOA posizioaJokalaria);
 POSIZIOA ZIRKULUA_JOKALARIARA_MUGITU2(POSIZIOA posizioaPilota, POSIZIOA posizioaJokalaria);
-POSIZIOA saguarenPosizioa();
 EGOERA JOKO_EGOERA_AZTERTU_BOSS1B(int bizitzKopurua, int bossarenBizitzak);
 
 #endif
-

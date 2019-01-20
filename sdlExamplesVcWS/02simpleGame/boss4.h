7c64434d6dc8ec3dbf355f68a2bb10e40ea83cab
AitorAstorga <44289776+AitorAstorga@users.noreply.github.com>
2019-01-20 21:22:44 +0100

    Update boss4.h

--- sdlExamplesVcWS/02simpleGame/boss4.h
+++ sdlExamplesVcWS/02simpleGame/boss4.h
@@ -3,9 +3,8 @@
 
 #include "ourTypes.h"
 
-POSIZIOA ZIRKULUA_JOKALARIARA_MUGITU_BOSS4(POSIZIOA posizioaPilota, POSIZIOA posizioaJokalaria);
+POSIZIOA ZIRKULUA_JOKALARIARA_MUGITU_BOSS4(POSIZIOA posizioaPilota, POSIZIOA posizioaJokalaria, POSIZIOA boss4);
 EGOERA JOKOA_egoera(JOKO_ELEMENTUA jokalaria, JOKO_ELEMENTUA oztopoa, int tiroImage);
-POSIZIOA saguarenPosizioa();
 EGOERA JOKO_EGOERA_AZTERTU_BOSS4(int bizitzKopurua, int bossarenBizitzak);
 
 #endif

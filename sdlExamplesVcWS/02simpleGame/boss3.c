6406ac54fa788c5115a190010efcb347f9b417ab
AitorAstorga <44289776+AitorAstorga@users.noreply.github.com>
2019-01-20 21:20:55 +0100

    Update boss3.c

--- sdlExamplesVcWS/02simpleGame/boss3.c
+++ sdlExamplesVcWS/02simpleGame/boss3.c
@@ -1,16 +1,15 @@
+#include "jokalaria.h"
+#include "globals.h"
+#include "boss3.h"
 #include "imagen.h"
 #include "graphics.h"
 #include "ebentoak.h"
 #include "soinua.h"
 #include <stdio.h>
 #include <windows.h>
+#include "irudiakEtaSoinuak.h"
 #include <time.h>
 #include <stdlib.h>
-#include "boss3.h"
-#include "irudiakEtaSoinuak.h"
-#include "jokalaria.h"
-//#include "globals.h"//ebentu
-int ebentu;
 
 EGOERA boss3(void) {
 	EGOERA  egoera = BOSS3;
@@ -19,9 +18,9 @@ EGOERA boss3(void) {
 	int bizitzaKendu[5] = { 1,1,1,1,1 };
 	JOKO_ELEMENTUA jokalaria, boss3, tiroa;//koord: ARMA_BIRAKARIA_MUGITU bueltatzen duen balioa.
 	POSIZIOA koord, saguPos, armarenKokapena = { SCREEN_WIDTH / 2 - 17, SCREEN_HEIGHT / 2 - 18 };
-	float rad = 400, anguloa = 0, abiaduraBoss = 0.0115;//armaBirakaria-ren erradioa eta biraketaren hasierako anguloa
+	float rad = 400, anguloa = 0, abiaduraBoss = (float)0.0115;//armaBirakaria-ren erradioa eta biraketaren hasierako anguloa
 	int imageIdMapa, imageIdBala;
-	int kont = 0, moteldu = 1, norabidea = 1;//moteldu: Arma-ren abiadura motelago doa 'moteldu' handiagoekin.
+	int kont = 0, norabidea = 1;//moteldu: Arma-ren abiadura motelago doa 'moteldu' handiagoekin.
 	int hit1 = 0, hit2 = 0, inmuneKont = 0, inmune = 80;
 
 	boss3.pos.x = SCREEN_WIDTH / 2 - 48;
@@ -49,8 +48,8 @@ EGOERA boss3(void) {
 	bizitz5 = irudiaKargatu(PLAYER_HEART_IMAGE);
 
 	irudiaMugitu(imageIdMapa, 0, 0);
-	irudiaMugitu(boss3.id, boss3.pos.x, boss3.pos.y);
-	irudiaMugitu(jokalaria.id, jokalaria.pos.x, jokalaria.pos.y - 20);
+	irudiaMugitu(boss3.id, (int)boss3.pos.x, (int)boss3.pos.y);
+	irudiaMugitu(jokalaria.id, (int)jokalaria.pos.x, (int)jokalaria.pos.y - 20);
 	srand(time(NULL));
 	do {
 		Sleep(2);
@@ -61,26 +60,24 @@ EGOERA boss3(void) {
 		jokalaria.pos = JOKALARIAREN_MUGIMENDUA(jokalaria.pos);
 		jokalaria.id = JOKALARIAREN_IRUDIA(jokalaria.id, spriteAukeratu);
 		spriteAukeratu++;
-		//-------------------------------------------------------------------------------------
 		koord = ARMA_BIRAKARIA_MUGITU(koord, armarenKokapena, anguloa, rad);
 		norabidea = norabideaRandomizatu(norabidea);
 		if (norabidea == 1) anguloa -= abiaduraBoss;
 		else anguloa += abiaduraBoss;
-		//-------------------------------------------------------------------------------------
 		pantailaGarbitu();
 		if (bizitzKopurua > 0) {
 			bizitzKopurua = JOKALARIAREN_BIZITZA(bizitzKopurua, bizitz1, bizitz2, bizitz3, bizitz4, bizitz5, bizitzaKendu);
 		}
 		if (inmuneKont == 0) {
-			hit1 = kolisioaBoss3(jokalaria.pos, armarenKokapena, koord, anguloa, rad, bizitzKopurua);
+			hit1 = kolisioaBoss3(jokalaria.pos, armarenKokapena, koord, anguloa, rad);
 			if (hit1 == 1) {
 				inmuneKont += inmune;//Jokalariak kolpe bat jasotzen duenean denbora tarte batean inmunea izango da.
 				bizitzKopurua--;
 			}
 		}
 		else inmuneKont--;
-		irudiaMugitu(jokalaria.id, jokalaria.pos.x, jokalaria.pos.y - 20);
-		irudiaMugitu(imageIdBala, tiroa.pos.x, tiroa.pos.y);
+		irudiaMugitu(jokalaria.id, (int)jokalaria.pos.x, (int)jokalaria.pos.y - 20);
+		irudiaMugitu(imageIdBala, (int)tiroa.pos.x, (int)tiroa.pos.y);
 		armaMarraztu(armarenKokapena, koord, anguloa, rad);
 		pantailaBerriztu();
 		egoera = JOKO_EGOERA_AZTERTU_BOSS3(bizitzKopurua, bossarenBizitzak);
@@ -94,8 +91,8 @@ EGOERA boss3(void) {
 }
 
 POSIZIOA ARMA_BIRAKARIA_MUGITU(POSIZIOA posizioa, POSIZIOA pos0, float anguloa, float rad) {
-	posizioa.x = rad * cos(anguloa) + pos0.x;
-	posizioa.y = rad * sin(anguloa) + pos0.y;
+	posizioa.x = rad * (float)cos(anguloa) + pos0.x;
+	posizioa.y = rad * (float)sin(anguloa) + pos0.y;
 	/*erradioa, anguloa eta zentroa-ren posizioa(pos0) emanda zirkunferentzia baten puntuak ematen ditu.
 	Funtzioa askotan deitzen angulo ezberdinekin zirkunferentzia bat sortuko da pos0-ren inguruan posizioa puntuekin.*/
 
@@ -111,14 +108,14 @@ int norabideaRandomizatu(int norabidea) {
 
 void armaMarraztu(POSIZIOA kokapena, POSIZIOA koord, float anguloa, float luzera) {
 	POSIZIOA posizioa;
-	int zirkuloDiametro = 26, j = luzera, zirkuloKopurua = luzera / zirkuloDiametro;
+	int zirkuloDiametro = 26, j = (int)luzera, zirkuloKopurua = (int)luzera / zirkuloDiametro;
 	int imageIdArmaArray[20];
 	for (int i = 0; i < zirkuloKopurua; i++) {
 		int imageIdArma = irudiaKargatu(IMAGE_ARMA_PULPO);
 		imageIdArmaArray[i] = imageIdArma;
 		j -= zirkuloDiametro;
 		posizioa = ARMA_BIRAKARIA_MUGITU(koord, kokapena, anguloa, luzera - j);
-		irudiaMugitu(imageIdArmaArray[i], posizioa.x, posizioa.y);
+		irudiaMugitu(imageIdArmaArray[i], (int)posizioa.x, (int)posizioa.y);
 	}
 	irudiakMarraztu();
 	for (int i = 0; i < zirkuloKopurua; i++) {
@@ -129,14 +126,16 @@ void armaMarraztu(POSIZIOA kokapena, POSIZIOA koord, float anguloa, float luzera
 int kolisioaBoss3(POSIZIOA jokalaria, POSIZIOA Kokapena, POSIZIOA koord, float anguloa, float luzera) {
 	POSIZIOA kolisioa;
 	int x, y, xJokalari = (int)jokalaria.x, yJokalari = (int)jokalaria.y;
+	int hit = 0;
 	for (int i = 0; i < luzera; i++) {
-		kolisioa = ARMA_BIRAKARIA_MUGITU(koord, Kokapena, anguloa, i);
+		kolisioa = ARMA_BIRAKARIA_MUGITU(koord, Kokapena, anguloa, (float)i);
 		x = (int)kolisioa.x;
 		y = (int)kolisioa.y;
 		if (xJokalari > x - 32 && xJokalari < x && yJokalari > y - 32 && yJokalari < y + 50) {
-			return 1;
+			hit = 1;
 		}
 	}
+	return hit;
 }
 
 EGOERA JOKO_EGOERA_AZTERTU_BOSS3(int bizitzKopurua, int bossarenBizitzak) {

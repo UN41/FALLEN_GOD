1f2800f28e496ac597502b899389eb773a0df008
AitorAstorga <44289776+AitorAstorga@users.noreply.github.com>
2019-01-20 21:18:26 +0100

    Update boss2b.h

--- sdlExamplesVcWS/02simpleGame/boss2b.h
+++ sdlExamplesVcWS/02simpleGame/boss2b.h
@@ -1,12 +1,15 @@
-#ifndef BOSS2_H
-#define BOSS2_H
+#ifndef BOSS2B_H
+#define BOSS2B_H
 
 #include "ourTypes.h"
 
 POSIZIOA BOSS_JOKALARIARA_JARRAITU(POSIZIOA posizioaBoss, POSIZIOA posizioaJokalaria);
-POSIZIOA saguarenPosizioa();
 int SUGEAREN_IRUDIA(int irudia);
 int SUGEA_GELDIRIK(int irudia);
+int SUGEAREN_IRUDIA_A(int irudiaA);
+int SUGEAREN_IRUDIA_B(int irudiaB);
+int SUGEA_GELDIRIK_A(int irudiaA);
+int SUGEA_GELDIRIK_B(int irudiaB);
 EGOERA JOKO_EGOERA_AZTERTU_BOSS2(int bizitzKopurua, int bossarenBizitzak);
 EGOERA JOKO_EGOERA_AZTERTU_BOSS2B(int bizitzKopurua, int bossarenBizitzak);
 
